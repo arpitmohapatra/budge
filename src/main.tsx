fcd47cd18994001c9127904705d09f0945b69f2b
Arpit Mohapatra <arpitmohapatra18@gmail.com>
2025-11-19 00:27:40 -0500

    Added install config

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Register service worker for PWA functionality
+import { registerSW } from 'virtual:pwa-register'
+
+// Register the service worker
+registerSW({ 
+  immediate: true,
+  onNeedRefresh() {
+    // You can add custom logic here if you want to prompt users to refresh
+    console.log('New content available, please refresh.')
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
